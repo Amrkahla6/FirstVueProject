1406708d492b7bdaa5c854770ea283c833558b59
Amr Kahla <amrkahla6@gmail.com>
2024-07-10 14:05:54 +0300

    Add layout header componant

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+    <the-header title="RememberMe"></the-header>
     <StoredResources :resources="storedResources"></StoredResources>
 </template>
 
@@ -6,12 +7,15 @@
 
 
 import StoredResources from './componants/learning-resources/StoredResources.vue';
+import TheHeader from './componants/layouts/TheHeader.vue';
+
 
 export default {
 
     components : {
 
-        StoredResources
+        StoredResources,
+        TheHeader
     },
    data () {
     return {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import BaseCard from './componants/UI/BaseCard.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+
+app.component('base-card',BaseCard)
+
+app.mount('#app');
